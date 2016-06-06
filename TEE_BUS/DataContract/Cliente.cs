82484b6bfad2d3f647342617218e6fa55debae7a
Geovanni Barreto <geovanni_barreto@hotmail.com>
2016-06-05 23:28:27 -0300

    Datacontracs

---
+++ TEE_BUS/DataContract/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace TEE_BUS.DataContracts
+{   
+    [DataContract]
+    public class Cliente
+    {
+        [DataMember]
+        public int ClienteID { get; set; }
+        [DataMember]
+        public string Nome { get; set; }
+        [DataMember]
+        public string Cnpj { get; set; }
+        [DataMember]
+        public string RazaoSocial { get; set; }
+        [DataMember]
+        public string Endereco { get; set; }
+    }
+}
